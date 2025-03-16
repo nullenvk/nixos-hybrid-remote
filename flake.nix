ae4195843ff0b79d2100efdd4502c0ea1a9a4c6a
nullenvk <nullenvk@protonmail.com>
2025-03-16 16:46:07 +0100

    Update to latest NixOS, add flake.nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A remote gaming module based on KDE Plasma and Sunshine";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosModules = {
+      hybrid-remote = import ./module.nix
+    };
+  };
+}
